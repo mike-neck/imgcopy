571485bfb985a9462bb2e8c1817713bfaba3ca99
mike-neck <jkrt3333@gmail.com>
2024-03-04 22:49:18 +0900

    Add "imgfile" project

--- Package.swift
+++ Package.swift
@@ -1,18 +1,29 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
         name: "imgcopy",
+        platforms: [.macOS(.v10_14)],
+        products: [
+            .executable(name: "imgcopy", targets: ["imgcopy"]),
+            .executable(name: "imgfile", targets: ["imgfile"]),
+        ],
         dependencies: [
             // Dependencies declare other packages that this package depends on.
             // .package(url: /* package url */, from: "1.0.0"),
+            .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         ],
         targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-            .target(
+            .executableTarget(
+                    name: "imgfile",
+                    dependencies: [
+                        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                    ]),
+            .executableTarget(
                     name: "imgcopy",
                     dependencies: ["ImgCopyMod"]),
             .target(
