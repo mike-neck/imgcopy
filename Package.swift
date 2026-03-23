f572b378b0b6b16bda3d29cb903b90fcfb91348c
mike-neck <jkrt3333@gmail.com>
2026-03-24 07:15:52 +0900

    Add `imgview` executable for image preview functionality
    - Introduced a new `imgview` executable with support for viewing images from file paths or clipboard.
    - Added `ViewMode` option to toggle between terminal and window display modes.

--- Package.swift
+++ Package.swift
@@ -9,11 +9,12 @@ let package = Package(
         products: [
             .executable(name: "imgcopy", targets: ["imgcopy"]),
             .executable(name: "imgfile", targets: ["imgfile"]),
+            .executable(name: "imgview", targets: ["imgview"]),
         ],
         dependencies: [
             // Dependencies declare other packages that this package depends on.
             // .package(url: /* package url */, from: "1.0.0"),
-            .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+            .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.1"),
         ],
         targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,6 +30,13 @@ let package = Package(
                     name: "imgcopy",
                     dependencies: ["ImgCopyMod"],
                     resources: [.copy("version.txt")]
+            ),
+            .executableTarget(
+                name: "imgview",
+                dependencies: [
+                    .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                ],
+                resources: [.copy("version.txt")]
             ),
             .target(
                     name: "ImgCopyMod",
