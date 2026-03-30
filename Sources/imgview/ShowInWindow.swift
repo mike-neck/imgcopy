d2399d57fc57fae57d2cf6a364b0cf618cf9ba5b
mike-neck <jkrt3333@gmail.com>
2026-03-31 07:44:41 +0900

    Add window-based image preview functionality to `ViewMode`.
    - Extended `ViewMode` enum with concurrency-safe `ImageConsumer` conformance and `show` function for image display.
    - Implemented `showInWindow` to support image viewing in a resizable macOS window.
    - Added `ImageViewWindow` class for handling window management and lifecycle.
    - Enhanced `Data` extension to parse image size and handle `NSImage` creation for display.

---
+++ Sources/imgview/ShowInWindow.swift
@@ -0,0 +1,64 @@
+import Cocoa
+import Foundation
+
+class ImageViewWindow: NSWindow, NSWindowDelegate {
+    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+    }
+
+    func windowWillClose(_ notification: Notification) {
+        NSApplication.shared.terminate(self)
+    }
+}
+
+extension Data {
+    func getSize() -> NSSize? {
+        return image?.size
+    }
+
+    var image: NSImage? {
+        get {
+            NSImage(data: self)
+        }
+    }
+}
+
+@MainActor
+func showInWindow(_ data: Data) throws {
+    let windowApp = NSApplication.shared
+    NSApp.setActivationPolicy(.regular)
+    guard let image = data.image else {
+        throw RuntimeError(description: "Unexpected data type for image")
+    }
+    let windowRect = getDefaultRect(image: image)
+    let imageView = NSImageView(image: image)
+    imageView.imageScaling = .scaleProportionallyUpOrDown
+    let window = ImageViewWindow(
+        contentRect: windowRect,
+        styleMask: [.titled, .closable, .resizable, .miniaturizable, .fullSizeContentView],
+        backing: .buffered,
+        defer: false
+    )
+    window.center()
+    window.title = "Image View"
+    window.contentView = imageView
+    window.makeKeyAndOrderFront(nil)
+    windowApp.run()
+}
+
+func getDefaultRect(image: NSImage) -> NSRect {
+    guard let mainScreen = NSScreen.main else {
+        return NSMakeRect(0, 25, image.size.width, image.size.height)
+    }
+    let mainFrame = mainScreen.frame
+    let size = mainFrame.size
+    let width = size.width
+    let height = size.height
+    return NSMakeRect(
+            width / 3 + 1,
+            height / 4 + 1.0,
+            image.size.width,
+            image.size.height
+    )
+}
+
