b407b90f72762a6e661e59e75d02865bec224722
mike-neck <jkrt3333@gmail.com>
2026-04-21 06:39:14 +0900

    Remove unused `URL` extension from `imgview` for cleanup
    - Deleted an unused `URL` extension that included custom initializers and a `text` property.
    - Improves code maintainability by removing redundant or dead code.

--- Sources/imgview/imgview.swift
+++ Sources/imgview/imgview.swift
@@ -90,33 +90,6 @@ extension ImgView {
 
 }
 
-extension URL {
-    init?(withUnknownFormatOf filePath: String) {
-        if filePath.hasPrefix("https://") || filePath.hasPrefix("http://") {
-            return nil
-        } else if filePath.hasPrefix("file://") {
-            guard let tempURL = URL(string: filePath) else {
-                return nil
-            }
-            self = tempURL
-        } else if filePath.hasPrefix("/") {
-            self = URL(fileURLWithPath: filePath)
-        } else {
-            self = URL(fileURLWithPath: FileManager().currentDirectoryPath).appendingPathComponent(filePath)
-        }
-    }
-
-    var text: String? {
-        get {
-            do {
-                return try String(contentsOf: self)
-            } catch {
-                return nil
-            }
-        }
-    }
-}
-
 public extension ImageSource {
     init(_ source: String?) {
         #if DEBUG
