7446a5e1c4bebb17c7a5d5dc320375d9c514af53
mike-neck <jkrt3333@gmail.com>
2026-04-22 07:27:18 +0900

    Move `URL` extension to `IOUtils` for improved modularity
    - Transferred the `URL` extension (with custom initializer and `text` property) from `imgfile` to `IOUtils`, centralizing shared utility code.
    - Removed the duplicate `URL` extension implementation from `imgfile` to eliminate redundancy.

--- Sources/IOUtils/URL-extension.swift
+++ Sources/IOUtils/URL-extension.swift
@@ -1,3 +1,28 @@
 import Foundation
 
+public extension URL {
+    init?(withUnknownFormatOf filePath: String) {
+        if filePath.hasPrefix("https://") || filePath.hasPrefix("http://") {
+            return nil
+        } else if filePath.hasPrefix("file://") {
+            guard let tempURL = URL(string: filePath) else {
+                return nil
+            }
+            self = tempURL
+        } else if filePath.hasPrefix("/") {
+            self = URL(fileURLWithPath: filePath)
+        } else {
+            self = URL(fileURLWithPath: FileManager().currentDirectoryPath).appendingPathComponent(filePath)
+        }
+    }
 
+    var text: String? {
+        get {
+            do {
+                return try String(contentsOf: self)
+            } catch {
+                return nil
+            }
+        }
+    }
+}
